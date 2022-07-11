ae6eca6e3a4416d8f5fb9ea094586831b7643658
Marcos Sanches <58822240+markitosanches@users.noreply.github.com>
2022-07-11 11:03:00 -0400

    Seance 17

---
+++ view/client-show.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Client</title>
+    <link rel="stylesheet" href="{{path}}css/style.css">
+</head>
+<body>
+<h2>Visualiser le client</h2>
+<p><strong>Nom : </strong>{{ client.nom }}</p>
+<p><strong>Adresse : </strong>{{ client['adresse'] }}</p>
+<p><strong>Phone : </strong>{{ client.phone }}</p>
+<p><strong>Ville : </strong> {{ ville }}</p>
+<p><strong><a href="{{path}}client/edit/{{client.id}}">Modifier</a></p>
+    
+</body>
+</html>
